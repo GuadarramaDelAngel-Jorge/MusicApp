e227e635313057d8108a06d5d154fea619875643
JORGE GUADARRAMA DEL ANGEL <jorgeg1997@hotmail.com>
2017-11-16 21:22:29 -0600

    Add media AudioManager for key sounds and SoundPool

--- app/src/main/java/guadarrama_jorge/musicalapp/MainActivity.java
+++ app/src/main/java/guadarrama_jorge/musicalapp/MainActivity.java
@@ -1,16 +1,92 @@
 package guadarrama_jorge.musicalapp;
 
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Helpful Constants
+    private final int NR_OF_SIMULTANEOUS_SOUNDS = 7;
+    // private final float LEFT_VOLUME = 1.0f;
+    //private final float RIGHT_VOLUME = 1.0f;
+    //private final int NO_LOOP = 0;
+    //private final int PRIORITY = 0;
+    //private final float NORMAL_PLAY_RATE = 1.0f;
+
+    // TODO: Add member variables here
+
+    private SoundPool mSoundPool;
+    private int mCSoundId;
+    private int mDSoundId;
+    private int mESoundId;
+    private int mFSoundId;
+    private int mGSoundId;
+    private int mASoundId;
+    private int mBSoundId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // TODO: Create a new SoundPool
+
+        mSoundPool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS,AudioManager.STREAM_MUSIC,0);
+
+        // TODO: Load and get the IDs to identify the sounds
+
+        mCSoundId = mSoundPool.load(getApplicationContext(), R.raw.note1_c, 1);
+        mDSoundId = mSoundPool.load(getApplicationContext(), R.raw.note2_d, 1);
+        mESoundId = mSoundPool.load(getApplicationContext(), R.raw.note3_e, 1);
+        mFSoundId = mSoundPool.load(getApplicationContext(), R.raw.note4_f, 1);
+        mGSoundId = mSoundPool.load(getApplicationContext(), R.raw.note5_g, 1);
+        mASoundId = mSoundPool.load(getApplicationContext(), R.raw.note6_a, 1);
+        mBSoundId = mSoundPool.load(getApplicationContext(), R.raw.note7_b, 1);
 
     }
 
+
+
+    // TODO: Add the play methods triggered by the buttons
+    public void playC(View view) {
+
+        Log.d("Xylophone","Red Button Clicked");
+
+        mSoundPool.play(mCSoundId,1.0f,1.0f,0,0,1.0f);
+    }
+
+
+    public void playD(View view) {
+        Log.d("Xylophone","Orange Button Clicked");
+        mSoundPool.play(mDSoundId,1.0f,1.0f,0,0,1.0f);
+    }
+
+    public void playE(View view) {
+        Log.d("Xylophone","Yellow Button Clicked");
+        mSoundPool.play(mESoundId,1.0f,1.0f,0,0,1.0f);
+    }
+
+    public void playF(View view) {
+        Log.d("Xylophone","Green Button Clicked");
+        mSoundPool.play(mFSoundId,1.0f,1.0f,0,0,1.0f);
+    }
+
+    public void playG(View view) {
+        Log.d("Xylophone","Turquoise Button Clicked");
+        mSoundPool.play(mGSoundId,1.0f,1.0f,0,0,1.0f);
+    }
+
+    public void playA(View view) {
+        Log.d("Xylophone","Blue Button Clicked");
+        mSoundPool.play(mASoundId,1.0f,1.0f,0,0,1.0f);
+    }
+
+    public void playB(View view) {
+        Log.d("Xylophone","Purple Button Clicked");
+        mSoundPool.play(mBSoundId,1.0f,1.0f,0,0,1.0f);
+    }
 }
